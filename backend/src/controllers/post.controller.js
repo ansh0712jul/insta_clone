66e2160d9015863b724421e39151d97f0d20349e
Ansh agrawal <agrawalansh624@gmail.com>
2025-02-06 23:20:34 +0530

    Create endpoint for post schema

--- backend/src/controllers/post.controller.js
+++ backend/src/controllers/post.controller.js
@@ -80,3 +80,134 @@ export const getAllPosts = asyncHandler(async (_, res) => {
 
     return res.status(200).json(new ApiResponse(200, posts, "Posts fetched successfully"));
 });
+
+// endpoint to get post posted by user 
+
+export const getPostsByUser = asyncHandler(async (req, res) => {
+
+    const userId = req.user._id;
+    if(!userId){
+        throw new ApiError(401,"unauthorized access")
+    }
+
+    const posts = await Post.find({author:userId})
+                    .sort({ createdAt: -1 })
+                    .populate({ path: "author", select: "username profileImg" })
+                    .populate({
+                        path: "comments",
+                        sort: { createdAt: -1 },
+                        populate: { path: "author", select: "username profileImg" }
+                    });
+
+    if (!posts || posts.length === 0) {
+        throw new ApiError(500, "Something went wrong while fetching posts");
+    }
+
+    return res.status(200).json(new ApiResponse(200, posts, "Posts fetched successfully"));
+                           
+})
+
+// endpoint to like a post 
+
+export const likeOrDislikePost = asyncHandler(async (req, res) => {
+    
+    const userId = req.user._id;
+    if(!userId){
+        throw new ApiError(401,"unauthorized access")
+    }
+
+    const postId = req.params.postId;
+    if(!postId){
+        throw new ApiError(400,"post id is required")
+    }
+
+    const post = await Post.findById(postId);
+    if(!post){
+        throw new ApiError(404,"post not found")
+    }
+    let msg = ""
+    if(post.likes.includes(userId)){
+
+        // $pull is used to remove an element from an array
+        await post.updateOne({$pull:{likes:userId}})
+        msg = "post disliked successfully"
+    }
+    else {
+
+        // $addToSet is used to add unique elements to an array
+        await post.updateOne({$addToSet:{likes:userId}})
+        msg = "post liked successfully"
+    }
+    await post.save({ValidateBeforeSave:false});
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,post,msg))
+})
+
+// endpoint to comment on a post 
+
+export const commentOnPost = asyncHandler(async (req, res) => {
+
+    const userId = req.user._id;
+    if(!userId){
+        throw new ApiError(401,"unauthorized access")
+    }
+
+    const postId = req.params.postId;
+    if(!postId){
+        throw new ApiError(400,"post id is required")
+    }
+
+    const { text } = req.body;
+    if(!text){
+        throw new ApiError(400,"comment is required")
+    }
+
+    const post = await Post.findById(postId);
+    if(!post){
+        throw new ApiError(404,"post not found")
+    }
+
+    const comment = await Comment.create({
+        text,
+        post:postId,
+        author:userId
+    });
+    await comment.populate({path:"author",select:"username profileImg"});
+
+    if(!comment){
+        throw new ApiError(500,"something went wrong while adding comment")
+    }
+
+    post.comments.push(comment._id);
+    await post.save({ValidateBeforeSave:false});
+
+    return res
+    .status(201)
+    .json(new ApiResponse(201,post ,"comment added successfully"))
+
+})
+
+// endpoint to get all comments on a post
+
+export const getAllComments = asyncHandler(async (req, res) => {
+
+    const postId = req.params.postId;
+    if(!postId){
+        throw new ApiError(400,"post id is required")
+    }
+
+    const comments = await Comment.find({post:postId})
+    .sort({ createdAt: -1 })
+    .populate({ path: "author", select: "username profileImg" });
+
+    if (!comments || comments.length === 0) {
+        throw new ApiError(500, "Something went wrong while fetching comments");
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200,comments,"comments fetched successfully"))
+})
+
--- backend/src/routes/post.routes.js
+++ backend/src/routes/post.routes.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
-import { addPost, getAllPosts } from "../controllers/post.controller.js";
+import { addPost, commentOnPost,  getAllComments, getAllPosts, getPostsByUser, likeOrDislikePost } from "../controllers/post.controller.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 import upload from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
+
+router.route("/get-all-posts").get(verifyJwt ,getAllPosts);
+router.route("/user/get-post").get(verifyJwt ,getPostsByUser);
+router.route("/:postId/get/comment-on-post").get(verifyJwt ,getAllComments)
 router.route("/add-post").post(verifyJwt, upload.single("image"), addPost);
-router.route("/get-all-posts").get(getAllPosts);
+router.route("/:postId/like-Or-Dislike-post").post(verifyJwt ,likeOrDislikePost);
+router.route("/:postId/comment-on-post").post(verifyJwt ,commentOnPost);
+
+
 
 export default router
